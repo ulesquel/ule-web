6509be426dad685d1c4e49cf4b1ca3f767661152
ElexemSDdR <valendomta@gmail.com>
2026-05-30 19:54:28 -0300

    Arreglando el problema anterior de tailwind y agregando importaciones en el layout a la fuente de Ubuntu, eliminando así la carga del cliente de las mismas fuentes locales

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,31 +1,10 @@
 // @ts-check
-import { defineConfig, fontProviders } from "astro/config";
+import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
-  },
-  fonts: [
-    {
-      provider: fontProviders.local(),
-      name: "Ubuntu",
-      cssVariable: "--font-ubuntu",
-      options: {
-        variants: [
-          {
-            src: ["./src/assets/fonts/ubuntu-font-family-0.83/Ubuntu-R.ttf"],
-            weight: "normal",
-            style: "normal",
-          },
-          {
-            src: ["./src/assets/fonts/ubuntu-font-family-0.83/Ubuntu-B.ttf"],
-            weight: "bold",
-            style: "normal",
-          },
-        ],
-      },
-    },
-  ],
+  }
 });
