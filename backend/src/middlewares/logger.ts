04e4797545e4984d9739b2b4d8edb08dbfa8c7c6
ElexemSDdR <valendomta@gmail.com>
2026-08-11 20:56:13 -0300

    Agregando un output más coloreado al logger

--- backend/src/middlewares/logger.ts
+++ backend/src/middlewares/logger.ts
@@ -1,16 +1,28 @@
 import { type Request, type Response } from 'express'
 import morgan from 'morgan'
 
-type Status = 200 | 300 | 400 | 500
+type Status = keyof typeof COLORED_CODES
+
+type Methods = keyof typeof COLORED_METHODS
+
+const COLORED_CODES = {
+  // <Status Code>: <colored ANSI code>
+  500: 31,
+  400: 33,
+  300: 36,
+  200: 32,
+}
+
+const COLORED_METHODS = {
+  GET: 36,
+  POST: 32,
+  PUT: 33,
+  DELETE: 31,
+  PATCH: 34,
+  QUERY: 35,
+}
 
 const colorStatus = (status: string | number | undefined) => {
-  const COLORED_CODES = {
-    // <Status Code>: <colored ANSI code>
-    500: 31,
-    400: 33,
-    300: 36,
-    200: 32,
-  }
   const code = Number(status) || 0
   const colorCode: (typeof COLORED_CODES)[keyof typeof COLORED_CODES] =
     COLORED_CODES[(Number(code.toString().split('')[0]) * 100) as Status] ?? 0
@@ -22,12 +34,27 @@ const colorStatus = (status: string | number | undefined) => {
   return `\x1b[${colorCode}m${code}\x1b[0m`
 }
 
+const colorMethod = (value: string | undefined) => {
+  if (!value) return '-'
+  const colorMethod = COLORED_METHODS[value as Methods] ?? 0
+
+  if (colorMethod === 0) return value
+
+  return `\x1b[1;${colorMethod}m${value}\x1b[0m`
+}
+
 const colorDate = (value: string | undefined) => {
   if (!value) {
     return '-'
   }
 
-  return `\x1b[35m${value}\x1b[0m`
+  return `\x1b[1;95m${value}\x1b[0m`
+}
+
+const colorUrl = (value: string | undefined) => {
+  if (!value) return '-'
+
+  return `\x1b[33m${value}\x1b[0m`
 }
 
 const morganOwnDevStyle: morgan.FormatFn<Request, Response> = (
@@ -35,9 +62,9 @@ const morganOwnDevStyle: morgan.FormatFn<Request, Response> = (
   req,
   res,
 ) => {
-  const method = tokens['method']!(req, res)
+  const method = colorMethod(tokens['method']!(req, res))
   const status = colorStatus(tokens['status']!(req, res))
-  const url = tokens['url']!(req, res)
+  const url = colorUrl(tokens['url']!(req, res))
   const length = tokens['res']!(req, res, 'content-length') || '-'
   const responseTime = tokens['response-time']!(req, res)
   const date = colorDate(tokens['date']!(req, res, 'web'))
