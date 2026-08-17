adf5849b0471034729f3f312af26e9c7b70d4b45
ElexemSDdR <valendomta@gmail.com>
2026-08-17 12:39:22 -0300

    Eliminando todo lo que sea frontend en el backend para dejarlo solo como una api

--- backend/public/services/dashboard.js
+++
@@ -1,8 +0,0 @@
-import { $, closeSession, renderCards } from '/services/functions.js'
-
-const $closeSession = $('#close-session')
-
-const allFestsRes = await fetch('/fests/')
-
-await renderCards(allFestsRes)
-$closeSession.addEventListener('click', closeSession)
